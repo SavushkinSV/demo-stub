160fb440619ed5613f57a59a24a0a24720bf693e
SavushkinSV <savushkin.sv@yandex.ru>
2025-04-25 12:44:08 +0300

    fix: name project

--- src/main/java/ssv/home/demostub/controller/StubController.java
+++ src/main/java/ssv/home/demostub/controller/StubController.java
@@ -1,6 +1,5 @@
 package ssv.home.demostub.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ssv.home.demostub.dto.ResponseDto;
 import ssv.home.demostub.dto.UserDto;
@@ -10,22 +9,18 @@ import ssv.home.demostub.util.RandomTime;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
 @RestController
 @RequestMapping("/user")
-public class StugController {
+public class StubController {
 
-    @GetMapping
+    @GetMapping()
     public UserStatusDto getUser() {
-//        log.info("GET");
 
         return new UserStatusDto("Login1", "Ok");
     }
 
-    @PostMapping
+    @PostMapping()
     public ResponseDto postUser(@RequestBody UserDto user) throws InterruptedException {
-//        log.info("POST");
-
         Thread.sleep(RandomTime.getRandomTime());
         String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
