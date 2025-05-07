bd4025d3d5822b9d9288bb4dd7008fc7dcea4f2d
SavushkinSV <savushkin.sv@yandex.ru>
2025-05-07 10:52:13 +0300

    fix: delete util class RandomTime

--- src/main/java/ssv/home/demostub/controller/StubController.java
+++ src/main/java/ssv/home/demostub/controller/StubController.java
@@ -4,24 +4,34 @@ import org.springframework.web.bind.annotation.*;
 import ssv.home.demostub.dto.ResponseDto;
 import ssv.home.demostub.dto.UserDto;
 import ssv.home.demostub.dto.UserStatusDto;
-import ssv.home.demostub.util.RandomTime;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/user")
 public class StubController {
 
+    private static final Random random = new Random();
+
     @GetMapping()
     public UserStatusDto getUser() {
-
+        try {
+            Thread.sleep(1000 + random.nextInt(1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return new UserStatusDto("Login1", "Ok");
     }
 
     @PostMapping()
-    public ResponseDto postUser(@RequestBody UserDto user) throws InterruptedException {
-        Thread.sleep(RandomTime.getRandomTime());
+    public ResponseDto postUser(@RequestBody UserDto user) {
+        try {
+            Thread.sleep(1000 + random.nextInt(1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
         return new ResponseDto(user.getLogin(), user.getPassword(), date);
